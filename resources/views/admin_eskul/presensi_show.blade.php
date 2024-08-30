266d6f3f71bbfb80b19d87a4d5b1a6115cb61a7d
myra45 <rianymawar156@gmail.com>
2024-08-30 15:05:48 +0700

    presensiiii

--- resources/views/admin_eskul/presensi_show.blade.php
+++ resources/views/admin_eskul/presensi_show.blade.php
@@ -71,38 +71,10 @@
     </div>
 
     <div class="border-bottom title-part-padding px-0 mb-3">
-        <h5>Buat Presensi</h5>
+        <h5>Quick Links</h5>
     </div>
     <div class="row">
-        <div class="col-md-9">
-            <div class="card">
-                <div class="card-body">
-                    <form method="POST" action="{{ route('admin_extracurricular_presensi_form_submit') }}">
-                        @csrf
-                        <input type="hidden" name="eskul_id" value="{{ $admin_data?->eskul_id }}">
-                        <div class="mb-3">
-                            <label for="eventName" class="form-label">Nama Event</label>
-                            <input type="text" class="form-control " id="eventName" name="nama_event">
-                        </div>
-                        <div class="mb-3">
-                            <label for="tanggal" class="form-label">Tanggal</label>
-                            <input type="date" class="form-control" id="tanggal" name="tanggal">
-                        </div>
-                        <div class="mb-3">
-                            <label for="jamMulai" class="form-label">Jam Mulai</label>
-                            <input type="time" class="form-control" id="jamMulai" name="jam_mulai">
-                        </div>
-                        <div class="mb-3">
-                            <label for="tempat" class="form-label">Tempat</label>
-                            <input type="text" class="form-control" id="tempat" name="tempat">
-                        </div>
-
-                        <button type="submit" class="btn btn-primary">Buat Presensi</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
+        {{-- <div class="col-md-12"> --}}
             <div class="card mb-3">
                 <div class="card-body">
                     <h4 class="mb-3">
@@ -123,7 +95,7 @@
                     <a href="" class="btn btn-primary">Export</a>
                 </div>
             </div>
-        </div>
+        {{-- </div> --}}
     </div>
 
 
