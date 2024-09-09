09f06e2ab39a57ef4c760e7ad3ad947c2fe6e1bd
myra45 <rianymawar156@gmail.com>
2024-09-09 23:46:18 +0700

    jjj

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers\Front;
 
 use App\Models\HomePageItem;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\HomePageItem;
 
 class HomeController extends Controller
 {
