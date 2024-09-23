3f29accb51527c0c95581a66f8ea845e022b8cb4
mujib967875 <mujibmy255@gmail.com>
2024-09-23 16:32:26 +0700

    p

--- resources/views/user/nilai_akhir.blade.php
+++ resources/views/user/nilai_akhir.blade.php
@@ -23,19 +23,13 @@
     <div class="section-body">
         <div class="row">
             <div class="col-md-12">
-                <div class="card">
-                    <div class="card-body">
-                        <h3 class="mb-3">Announcement</h3>
-                        <p class="fs-4">Pengumuman penting mengenai kegiatan ekstrakurikuler dapat ditampilkan di sini.</p>
-                    </div>
-                </div>
-                <h3>Final Score</h3>
+                
                 <div class="table-responsive">
                     <table class="table table-bordered" id="example1">
                         <thead>
                             <tr>
                                 <th>SL</th>
-                                <th>Nama Event</th>
+                                <th>Event Name</th>
                                 <th>Member Name</th>
                                 <th>extracurricular</th>
                                 <th>Final Scorer</th>
